5c44a7ad1089984feb6bf13d0c4a2b43e2d7c908
Bülent Vardal <bulentvardal@Bulent-MacBook-Pro.local>
2019-11-19 19:40:41 +0300

    TCP socket example added

---
+++ TCPSocketClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClientA.Connect("127.0.0.1","01010120210");
+        }
+    }
+}
---
+++ TCPSocketServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPSocketServer
+{
+    class Program
+    {
+        public static TcpListener server=null;
+
+        static void Main(string[] args)
+        {
+           try
+           {
+                Int32 port=1234;
+                IPAddress localAddress=IPAddress.Parse("127.0.0.1");
+                server=new TcpListener(localAddress,port);
+                server.Start();
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                while(true)
+                {
+                    System.Console.WriteLine("Bağlantı Bekleniyor.");
+                    TcpClient client=server.AcceptTcpClient();
+                    System.Console.WriteLine("Bağlandı");
+                    data=null;
+                    NetworkStream stream=client.GetStream();
+                    int i;
+                    while((i = stream.Read(bytes, 0, bytes.Length))!=0) 
+                    {   
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Geldi: {0}", data);
+                    
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Gitti: {0}", data);            
+                    }
+                    client.Close();
+                }
+
+           }
+           catch (SocketException e)
+           {
+               System.Console.WriteLine($"Hata :{e} ");
+           } finally{
+               server.Stop();
+           }
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
