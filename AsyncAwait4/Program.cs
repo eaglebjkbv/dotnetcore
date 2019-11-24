43dc33e059769d1ee5843b9fc7a5c88f6286f348
Bülent Vardal <bulentvardal@Bulent-MacBook-Pro.local>
2019-11-24 12:26:51 +0300

    Async Await 4th example added

---
+++ AsyncAwait4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwait4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await BekleAsync();
+            System.Console.WriteLine("Bitti");
+
+
+        }
+
+        static Task<string> BekleAsync(){
+            return Task.Run(()=>{
+               System.Threading.Thread.Sleep(1500);
+                return "bitti";
+            });
+        }
+    }
+}
