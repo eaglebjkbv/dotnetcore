5943c2446acaf9148c1000f3b526dfdbab4e4eb4
Bülent Vardal <bulentvardal@Bulent-MacBook-Pro.local>
2019-11-24 13:31:47 +0300

    New Http Cliesnt Async example added

---
+++ HttpClientJsonGet/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ConsoleTables;
+using Newtonsoft.Json;
+
+namespace HttpClientJsonGet
+{
+    class Model
+    {
+        public int userId { get; set; } 
+        public int id   { get; set; }
+        public string title  { get; set; }
+        public string body { get; set; }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient httpClient=new HttpClient();
+            HttpResponseMessage httpResponseMessage=await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+            httpResponseMessage.EnsureSuccessStatusCode();
+            string responseBody=await httpResponseMessage.Content.ReadAsStringAsync();
+            var result=JsonConvert.DeserializeObject<List<Model>>(responseBody); 
+            var table=new ConsoleTable("User id","id");
+
+            foreach (var item in result){
+                table.AddRow(item.userId,item.id);
+            }
+            table.Write();
+            {
+                
+            }
+        }
+    }
+}
