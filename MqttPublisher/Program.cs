2243e81b88ed505778050e8ee229629916a08831
Bülent Vardal <bulentvardal@Bulent-MacBook-Pro.local>
2020-04-29 17:08:43 +0300

    Some changes on MqttPublisher

--- MqttPublisher/Program.cs
+++ MqttPublisher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using uPLibrary.Networking.M2Mqtt.Exceptions;
 using uPLibrary.Networking.M2Mqtt.Messages;
 using uPLibrary.Networking.M2Mqtt.Session;
@@ -13,27 +14,34 @@ namespace Mttq_Client1
 {
     class Program
     {
+        static MqttClient client;
         static void Main(string[] args)
         {
-            MqttClient client = new MqttClient("mqtt.eclipse.org");
-            byte code = client.Connect(Guid.NewGuid().ToString());
-            
-             // publish olduğunda çağırılacak method.Aşağıda
+            Console.WriteLine("Durdurmak için bir tuşa bsınız...");
+            client = new MqttClient("mqtt.eclipse.org");
+          
 
+            // publish olduğunda çağırılacak method.Aşağıda
+            client.MqttMsgPublished += client_MqttMsgPublished;
+
+            byte code = client.Connect(Guid.NewGuid().ToString());
             ushort msgId = client.Publish("ev/odasicakligi",
-           Encoding.UTF8.GetBytes("Selamlar"),
+           Encoding.UTF8.GetBytes("Yeni Mesaj 0"),
            MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE,
            false);
-           client.MqttMsgPublished += client_MqttMsgPublished;
-          
+            Console.ReadKey(true);
+            if(client.IsConnected){
+                    client.Disconnect();
+
+            }
         }
-    // Bu Alt kısım nedense Hiç devreye girmiyor...
         static void client_MqttMsgPublished(object sender, MqttMsgPublishedEventArgs e)
         {
-            Debug.WriteLine("MessageId = " + e.MessageId + " Published = " + e.IsPublished);
-
-
-
+            Console.WriteLine("MessageId = " + (e.MessageId -1)+ " Published = " + e.IsPublished);
+            ushort msgId = client.Publish("ev/odasicakligi",
+           Encoding.UTF8.GetBytes("Yeni Mesaj"+ e.MessageId.ToString()),
+           MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE,
+           false);
         }
     }
 }
